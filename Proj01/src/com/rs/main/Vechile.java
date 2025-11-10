04147efea103946b773e03458b3e2f14febfb471
Rahul8080 <shuklarahul8085@gmail.com>
2025-11-10 13:04:42 +0530

    new second commit

--- Proj01/src/com/rs/main/Vechile.java
+++ Proj01/src/com/rs/main/Vechile.java
@@ -5,6 +5,7 @@ public class Vechile {
 	public static void main(String[] args) {
 		System.out.println("Vehicle is started");
 		System.out.println("Vehicle is stopped");
+		System.out.println("vehicle again started");
 	}
 
 }
