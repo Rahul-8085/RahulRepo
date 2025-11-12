a5d5c8afcfe80084e1ab110685228b6b45654200
Rahul8080 <shuklarahul8085@gmail.com>
2025-11-12 14:27:33 +0530

    TL create new DigitalBranch

---
+++ GITProj01/src/main/java/com/rs/digitalPayment/Digital.java
@@ -0,0 +1,7 @@
+package com.rs.digitalPayment;
+
+public class Digital {
+public String  SendMoney(long accno,double amt,String address) {
+	return amt+" is transfer to account no "+accno+" and address is "+address;
+}
+}
--- GITProj01/src/main/java/com/rs/main/App.java
+++ GITProj01/src/main/java/com/rs/main/App.java
@@ -1,7 +1,7 @@
 package com.rs.main;
 
 import com.rs.arth.Airthmetic;
-import com.rs.payment.UpiPayment;
+import com.rs.digitalPayment.Digital;
 import com.rs.payment.UpiPayment;
 
 public class App {
@@ -9,8 +9,11 @@ public class App {
 		Airthmetic arth = new Airthmetic();
 		System.out.println("Result is: " + arth.Sum(10, 20));
 		System.out.println("Result is: " + arth.Sub(10, 20));
-
+		System.out.println("=======================");
 		UpiPayment payment=new UpiPayment();
 		System.out.println(payment.Upi(52684971l, 5000.0));
+		System.out.println("=======================");
+		Digital dig=new Digital();
+		System.out.println(dig.SendMoney(541287964l, 50000.0, "Birsinghpur"));
 	}
 }
