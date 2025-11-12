d397dd89f03f17e6257450809f492b749ef88bfd
Rahul8080 <shuklarahul8085@gmail.com>
2025-11-12 13:57:22 +0530

    TL Create New Project

---
+++ GITProj01/src/main/java/com/rs/main/App.java
@@ -0,0 +1,13 @@
+package com.rs.main;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
