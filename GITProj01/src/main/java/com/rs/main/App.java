e84b5d85f921dbd712f554d3929e7bddaba2843a
Rahul8080 <shuklarahul8085@gmail.com>
2025-11-12 14:16:16 +0530

    TL create UpiBranch

--- GITProj01/src/main/java/com/rs/main/App.java
+++ GITProj01/src/main/java/com/rs/main/App.java
@@ -1,13 +1,16 @@
 package com.rs.main;
 
 import com.rs.arth.Airthmetic;
+import com.rs.payment.UpiPayment;
+import com.rs.payment.UpiPayment;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
-       Airthmetic arth=new Airthmetic();
-   System.out.println("Result is: "+arth.Sum(10, 20));
-   System.out.println("Result is: "+arth.Sub(10, 20));
-    }
+public class App {
+	public static void main(String[] args) {
+		Airthmetic arth = new Airthmetic();
+		System.out.println("Result is: " + arth.Sum(10, 20));
+		System.out.println("Result is: " + arth.Sub(10, 20));
+
+		UpiPayment payment=new UpiPayment();
+		System.out.println(payment.Upi(52684971l, 5000.0));
+	}
 }
---
+++ GITProj01/src/main/java/com/rs/payment/UpiPayment.java
@@ -0,0 +1,8 @@
+package com.rs.payment;
+
+public class UpiPayment {
+public String Upi(long accno, double amt) {
+	return +amt+"Ammount transfer to account no.: "+accno;
+	
+}
+}
